4efa4d043def8989b9dab4f3f364fd8970075197
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-11-16 19:06:36 +0500

    Task2

---
+++ Tyuiu.LyaginVA.Sprint5.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.LyaginVA.Sprint5.Task2.V1.Lib;
+
+namespace Tyuiu.LyaginVA.Sprint5.Task2.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\vova\source\repos\Tyuiu.LyaginVA.Sprint5\Tyuiu.LyaginVA.Sprint5.Task2.V1\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
