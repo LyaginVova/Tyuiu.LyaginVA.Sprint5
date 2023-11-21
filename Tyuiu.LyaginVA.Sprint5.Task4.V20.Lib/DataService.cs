532ce1d14171cfd33b13ed7c3e9915443506b31e
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-11-21 18:00:22 +0500

    Task4

---
+++ Tyuiu.LyaginVA.Sprint5.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LyaginVA.Sprint5.Task4.V20.Lib
+{
+    public class DataService : ISprint5Task4V20
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round((Math.Pow((((Math.Pow((Convert.ToDouble(strX)), 2)) / (Math.Sin(Convert.ToDouble(strX))))), 3)),3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.LyaginVA.Sprint5.Task4.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LyaginVA.Sprint5.Task4.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LyaginVA.Sprint5.Task4.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("421de778-4ee3-4437-8c75-955a19df4c21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
