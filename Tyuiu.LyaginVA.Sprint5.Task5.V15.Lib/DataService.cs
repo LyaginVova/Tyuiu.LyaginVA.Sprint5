5f17cb5df618b6d918322d324d26b485dbdb4180
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-11-22 19:55:39 +0500

    Task5

---
+++ Tyuiu.LyaginVA.Sprint5.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LyaginVA.Sprint5.Task5.V15.Lib
+{
+    public class DataService : ISprint5Task5V15
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 10000;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    foreach (char element in line)
+                    {
+                        if (element == ',')
+                        {
+                            if (Convert.ToDouble(line) % 5 == 0 && res > Convert.ToDouble(line))
+                            {
+                                res = Convert.ToDouble(line);
+                                res = Math.Round(res, 3);
+                                
+
+                            }
+                        }
+
+                    }
+                    
+                }
+            }
+            return res;
+            
+        }
+    }
+}
---
+++ Tyuiu.LyaginVA.Sprint5.Task5.V15/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.LyaginVA.Sprint5.Task5.V15.Lib;
+
+namespace Tyuiu.LyaginVA.Sprint5.Task5.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Лягин В. А. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Лягин Владимир Александрович | АСОиУБ-23-2                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask5V15.txt (файл взять из архива *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:|DataSprint5| и      *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор значение. Найти            *");
+            Console.WriteLine("* минимальное вещественное число в файле, которое делится на 5.           *");
+            Console.WriteLine("* Полученный результат вывести на консоль. У вещественных значений        *");
+            Console.WriteLine("* округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V15.txt";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Минимальное вещественное число в файле, которое делится на 5 = " + res);
+            Console.ReadKey();
+        }
+    }
+}
