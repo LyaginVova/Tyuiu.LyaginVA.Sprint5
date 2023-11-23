4615de6f43ebbdddd8baefbed0c880533f3c0f48
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-11-23 20:51:48 +0500

    Task6

---
+++ Tyuiu.LyaginVA.Sprint5.Task6.V5.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LyaginVA.Sprint5.Task6.V5.Lib
+{
+    public class DataService : ISprint5Task6V5
+    {
+        public int LoadFromDataFile(string path)
+        { 
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                Regex regex = new Regex("([A-Z])");
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    count += regex.Matches(line).Count;
+                }
+                
+
+
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.LyaginVA.Sprint5.Task6.V5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LyaginVA.Sprint5.Task6.V5.Lib;
+
+namespace Tyuiu.LyaginVA.Sprint5.Task6.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Лягин В. А. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Лягин Владимир Александрович | АСОиУБ-23-2                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask6V5.txt (файл взять из архива  *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:|DataSprint5| и      *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор символьных данных. Найти   *");
+            Console.WriteLine("* количество заглавных латинских букв в заданной строке.                 * ");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask6V5.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Количество заглавных латинских букв в данном файле = " + res);
+            Console.ReadKey();
+        }
+    }
+}
